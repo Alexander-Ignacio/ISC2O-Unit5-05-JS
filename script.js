7247087fd26331339544f1aca3ee1d20a7baa5dd
AlexanderIgnac1 <>
2021-04-30 13:34:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let length = 0
+let width = 0
+let number = 0
+let sentence = 'hello, this is the number 3.'
+
+document.getElementById('button').addEventListener('click', multiplication)
+
+function multiplication () {
+  length = document.getElementById('input').value
+  width = document.getElementById('input2').value  
+
+  length = parseInt(length)
+  width = parseInt(width)
+
+  number = length * width
+
+  alert(number)
+}
